92baed52e25ccbc9de4fcef01cc9d36082c9989a
PJSalter <45575016+PJSalter@users.noreply.github.com>
2023-10-10 15:02:08 +0100

    Declare module 'perlin-noise' with Perlin noise generation functions

---
+++ src/perlin-noise.d.ts
@@ -0,0 +1,5 @@
+declare module "perlin-noise" {
+  export function generatePerlinNoise(x: number, y: number): number;
+  export function generatePerlinNoise(x: number, y: number, z: number): number;
+  // Add other methods or types if needed
+}
